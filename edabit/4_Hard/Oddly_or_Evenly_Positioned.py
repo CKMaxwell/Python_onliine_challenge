56be14fb02684b7e9d06313179ef9e277e67d272
CKMaxwell <39400519+CKMaxwell@users.noreply.github.com>
2020-10-02 22:25:34 +0800

    20201002

---
+++ edabit/4_Hard/Oddly_or_Evenly_Positioned.py
@@ -0,0 +1,8 @@
+# 20201002 - Oddly or Evenly Positioned
+def char_at_pos(r, s):
+    if type(r) == list:
+        ans = []
+        if s == "even":
+            for i in range
+    elif type(r) == str:
+        ans = ""
